9a4379e40cae316b67db30ce61b276a57d470c92
Denis Golubev <maxmadnes@mail.ru>
2022-05-05 14:36:01 +0300

    add menu and form style

--- js/slider.js
+++ js/slider.js
@@ -7,16 +7,20 @@ const sliderWrapper= document.querySelector('.slider-wrapper');
 const btnShowTarifs = document.querySelector('.btn-main');
 const radioDots = document.querySelectorAll('.radio');
 const closeFormBtn = document.querySelector('.close-form-btn');
-const formModal = document.querySelector('.form-wrapper');
+const formModal = document.querySelector('.write-popup');
 const tarifsBtn = document.querySelectorAll('.btn-tarifs');
 const tarifsBtnNav =document.querySelector('#tarifs-link');
 const reviewsSlideBtn =document.querySelector('#reviews-link');
 const formContainer = document.querySelector('.callback-form');
 const inputs = formContainer.querySelectorAll('input');
+const showNavMenuBtn = document.querySelector('.burger-menu-btn');
+const navigationPanel = document.querySelector('.site-navigation');
 
 
 let activeSlide = 0;
 let width;
+
+/*Инициализация слайдера*/
 
 function init(){
   width = document.querySelector('.slider').offsetWidth;
@@ -28,11 +32,9 @@ function init(){
   rollSlider();
 }
 
-window.addEventListener('resize', init);
-init();
-
-rigthSlideArrow.addEventListener('click',rollRight);
-leftSlideArrow.addEventListener('click',rollLeft);
+function rollSlider(){
+  sliderWrapper.style.transform = 'translate(-'+activeSlide*width+ 'px';
+}
 
 function rollRight() {
   activeSlide++;
@@ -46,8 +48,6 @@ function rollRight() {
   rollSlider();
 };
 
-
-
 function rollLeft(){
   activeSlide--;
 
@@ -59,7 +59,6 @@ function rollLeft(){
   setActiveDots(activeSlide);
   rollSlider();
 };
-
 
 function setActiveSlide(n) {
   slides.forEach((slide) => {
@@ -76,10 +75,6 @@ function setActiveDots(n) {
   rollSlider();  
 }
 
-function rollSlider(){
-  sliderWrapper.style.transform = 'translate(-'+activeSlide*width+ 'px';
-}
-
 radioDots.forEach((item, indexDot) => {
   item.addEventListener('click', ()=>{
     activeSlide = indexDot;
@@ -88,6 +83,12 @@ radioDots.forEach((item, indexDot) => {
   })
   
 })
+
+
+rigthSlideArrow.addEventListener('click',rollRight);
+leftSlideArrow.addEventListener('click',rollLeft);
+
+/*Прокрутка к блоку при клике по ссылке */
 
 function slideToBlock(item){
   item.addEventListener("click",(e)=>{
@@ -100,6 +101,7 @@ function slideToBlock(item){
     })
 }
 
+/*Модальное окно формы */
 
 function showModal(e){
   e.preventDefault();
@@ -130,10 +132,29 @@ function resetInputs(elements){
   return;
 }
 
+/*Показать меню навигации*/
+
+function showMenu(){
+  navigationPanel.classList.toggle('menu-show');
+  const btnClose = document.createElement('button');
+  btnClose.className ='closeMenu';
+  btnClose.innerText='Закрыть меню';
+  navigationPanel.appendChild(btnClose);
+  btnClose.addEventListener('click',()=>{
+    navigationPanel.classList.remove('menu-show');
+    navigationPanel.removeChild(btnClose);
+  })
+}
+
+/*Вызов функций */
+
 slideToBlock(btnShowTarifs);
 slideToBlock(tarifsBtnNav);
 slideToBlock(reviewsSlideBtn);
+showNavMenuBtn.addEventListener('click', showMenu);
 
 closeForm();
+window.addEventListener('resize', init);
+init();
 
 })();
